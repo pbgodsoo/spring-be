5bffc0ebf68fd449e551275892cc0b5bbdc4d9fe
Beomsoo <linoleca@naver.com>
2026-02-24 09:26:11 +0900

    [feat] spring project initial commit

---
+++ src/main/java/org/example/springbe/SpringBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springbe/SpringBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
